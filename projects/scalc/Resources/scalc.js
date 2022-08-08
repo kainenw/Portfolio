a8e88aef1e334e53e4f55918685d3f535ff96e8f
kainenw <kainen.white@gmail.com>
2022-08-08 15:02:39 -0500

    fixes imperfections in home image

--- projects/scalc/Resources/scalc.js
+++ projects/scalc/Resources/scalc.js
@@ -5,14 +5,14 @@ const getInc = () => {
         income = incomeElement.valueAsNumber;
     }
     return income;
-};
+}
 
 function getExp() {
     let expense = document.getElementsByClassName("expense");
     let expenses = [0];
     const sum = (num1, num2) => {
         return num1 + num2;
-    };
+    }
     for (let i = 0; i < expense.length; i++) {
         let value = expense[i].valueAsNumber;
         if (value > 0) {
@@ -27,7 +27,7 @@ const print = () => {
     let total = getInc() - getExp();
     if(total > 0) {
         result.innerHTML = `You could add<br> <span>$${total}</span><br> to your savings each month!`;
-        result.getElementsByTagName('span')[0].style.color = "forestgreen";
+       result.getElementsByTagName('span')[0].style.color = "forestgreen";
     }else if(total < 0) {
         absVal = Math.abs(total);
         result.innerHTML = `You would lose<br> <span>$${absVal}</span><br> from your savings each month!`;
@@ -35,7 +35,7 @@ const print = () => {
     }else if(total === 0) {
         result.innerHTML = "You wouldn't be able to save any money on this budget.";
     }
-};
+}
 
 let button = document.getElementById('button');
 button.addEventListener('click', print);
