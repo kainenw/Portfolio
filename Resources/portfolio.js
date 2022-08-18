0264fd2ed67156da7537426ca27ca86080f978ba
kainenw <kainen.white@gmail.com>
2022-08-18 16:48:02 -0500

    adds motiquotes project to portfolio

--- Resources/portfolio.js
+++ Resources/portfolio.js
@@ -1,13 +1,24 @@
 let scalcLabel = document.getElementById('scalc');
+let motiLabel = document.getElementById('motiquotes')
 
-let descStyle = document.getElementById('scalcDesc').style;
+let scalcDesc = document.getElementById('scalcDesc');
+let motiDesc = document.getElementById('motiDesc')
 
-const displayScalc = () => {
-  if(descStyle.display === 'block') {
-    descStyle.display = 'none';
+const display = (element) => {
+  if(element.style.display === 'block') {
+    element.style.display = 'none';
   }else{
-    descStyle.display = 'block';
+    element.style.display = 'block';
   }
 };
 
+const displayScalc = () => {
+  return display(scalcDesc);
+}
+
+const displayMoti = () => {
+  return display(motiDesc);
+};
+
 scalcLabel.addEventListener('click', displayScalc);
+motiLabel.addEventListener('click', displayMoti);
