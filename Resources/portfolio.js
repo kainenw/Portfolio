e19231ab789d7555829063d2785e65c7bbc9c5ca
kainenw <kainen.white@gmail.com>
2022-09-22 17:36:16 -0500

    update project images and descriptions

--- Resources/portfolio.js
+++ Resources/portfolio.js
@@ -1,13 +1,45 @@
-let projects = document.getElementsByClassName('project');
-let desc = document.getElementsByClassName('desc');
+const proj1 = $('#proj1');
+const desc1 = $('#desc1');
+const proj2 = $('#proj2');
+const desc2 = $('#desc2');
+let proj3 = $('#proj3');
+let desc3 = $('#desc3');
 
-const display = (i) => {
-  for(let j = 0; j < projects.length; j++) {
-    desc[j].style.display = 'none';
-  }
-  desc[i].style.display === 'block' ? desc[i].style.display = 'none' : desc[i].style.display = 'block';
+/* let scroll;
+
+function setScroll() {
+    let header = document.querySelector('header');
+    scroll = scrollY;
+    if(scroll <= 0) {
+      header.style.opacity = '1';
+    } else {
+      header.style.opacity = '0.75';
+    }
 }
 
-for(let i = 0; i < projects.length; i++) {
-  projects[i].addEventListener('click', () => {display(i)});
-}
+
+
+document.addEventListener('scroll', setScroll); */
+
+
+
+proj1.on('click', () => {
+  desc1.toggle();
+});
+
+proj2.on('click', () => {
+  desc1.hide();
+  desc2.toggle();
+});
+
+proj3.on('click', () => {
+  desc1.hide();
+  desc3.toggle();
+});
+
+
+
+/* for(let i = 0; i < projects.length; i++){
+  console.log(desc[i]);
+  projects[i].addEventListener('click', toggle(i));
+} */
