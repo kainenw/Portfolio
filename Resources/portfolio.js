5b40f1a96a2d4001ba3a593a512eab9a257ec8e5
kainenw <108428180+kainenw@users.noreply.github.com>
2023-03-06 13:39:53 -0600

    create new branch, combine old and new design, add sections to function as cards, add functions to switch between cards

--- Resources/portfolio.js
+++ Resources/portfolio.js
@@ -1,45 +1,22 @@
-const proj1 = $('#proj1');
-const desc1 = $('#desc1');
-const proj2 = $('#proj2');
-const desc2 = $('#desc2');
-let proj3 = $('#proj3');
-let desc3 = $('#desc3');
+var slideIndex = 1;
+showDivs(slideIndex);
 
-/* let scroll;
-
-function setScroll() {
-    let header = document.querySelector('header');
-    scroll = scrollY;
-    if(scroll <= 0) {
-      header.style.opacity = '1';
-    } else {
-      header.style.opacity = '0.75';
-    }
+function plusDivs(n) {
+  showDivs((slideIndex += n));
 }
 
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("card");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
 
-
-document.addEventListener('scroll', setScroll); */
-
-
-
-proj1.on('click', () => {
-  desc1.toggle();
-});
-
-proj2.on('click', () => {
-  desc1.hide();
-  desc2.toggle();
-});
-
-proj3.on('click', () => {
-  desc1.hide();
-  desc3.toggle();
-});
-
-
-
-/* for(let i = 0; i < projects.length; i++){
-  console.log(desc[i]);
-  projects[i].addEventListener('click', toggle(i));
-} */
