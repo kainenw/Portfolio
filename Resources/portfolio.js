70a4eb5c2b0be0505b10479b106bd5ecdf98b294
kainenw <108428180+kainenw@users.noreply.github.com>
2023-04-12 16:28:24 -0500

    redesign projects section

--- Resources/portfolio.js
+++ Resources/portfolio.js
@@ -1,6 +1,44 @@
-const projs = document.getElementsByClassName("project")
+/* const header = document.querySelector("header");
+const slideContainer = document.getElementById("slide-container");
+const about = document.getElementById("about");
 
-for(let i = 0; i < projs.length; i++){
-  console.log(desc[i]);
-  projs[i].addEventListener('click', toggle(i));
+let minimized = false;
+
+const minimizeHeader = (event) => {
+  minimized = window.scrollY < 0;
+  if (minimized) {
+    header.style.top = "0";
+    slideContainer.style.top = "22rem";
+    slideContainer.style.overflow = "scroll";
+    minimized = false;
+  } else if (true) {
+    header.style.top = "-22rem";
+    slideContainer.style.top = "0";
+    slideContainer.style.overflow = "auto";
+    about.scrollTop = 0;
+    minimized = true;
+  }
+};
+
+document.body.addEventListener("scroll", minimizeHeader);
+slideContainer.addEventListener("scroll", minimizeHeader); */
+
+//new card js
+
+const options = document.getElementsByClassName("option");
+
+let prevActive = options[0];
+
+const activate = (event) => {
+  prevActive.classList.remove("active");
+  event.currentTarget.classList.add("active");
+  prevActive = event.currentTarget;
+};
+
+for (let option of options) {
+  option.addEventListener("click", activate);
 }
+
+let arr = [1, 2, 3, 4, 5];
+
+console.log(arr[-1]);
