e19081b2497cd57b5b85078785481b93a6a82d90
kainenw <108428180+kainenw@users.noreply.github.com>
2023-07-31 19:05:00 -0500

    change dark/light icons, rework css-in-js functs

--- src/index.js
+++ src/index.js
@@ -1,50 +1,96 @@
 const queryString = window.location.href;
-// change from 'index.html' to 'projects.html' if the projects section is moved
+// TODO change from 'index.html' to 'projects.html' if the projects section is moved
 const isHome = queryString.includes("contact.html");
-console.log(queryString);
-console.log(!isHome);
 
-if (!isHome) {
-  let projectMenu = document.getElementById("project-menu");
-  const buttons = projectMenu.querySelectorAll("button");
-
-  const projects = document.getElementsByClassName("project");
-
-  let prevActive = buttons[0];
-  let prevValue = 0;
-
-  const activate = (event) => {
-    const index = event.target.value;
-    prevActive.classList.remove("active");
-    projects[prevValue].classList.remove("active");
-    event.currentTarget.classList.add("active");
-    projects[index].classList.add("active");
-    prevActive = event.currentTarget;
-    prevValue = index;
-  };
-
-  for (const button of buttons) {
-    button.addEventListener("click", activate);
-  }
-}
-
-// TODO css styling for light mode
-// toggle dark/light mode
-let lightMode = false;
-
-const body = document.body;
-const toggleLight = document.getElementById("toggle-light");
-
-const toggle = (event) => {
-  if (lightMode) {
-    lightMode = false;
-    body.classList.remove("light");
-    event.target.src = "src/Img/dark/dark-icon.png";
-  } else {
-    lightMode = true;
-    body.classList.add("light");
-    event.target.src = "src/Img/light/light-icon.png";
-  }
+// colors
+const hex = {
+  backgroundLight: "#f2f2eb",
+  light: "#f2f2eb",
+  mediumLight: "#bfbfba",
+  medium: "#737372",
+  mediumDark: "#404040",
+  backgroundDark: "#262626",
+  dark: "#262626",
 };
 
-toggleLight.addEventListener("click", toggle);
+const hideShow = (hide, show) => {
+  hide.hide();
+  show.show();
+};
+
+// TODO #top border change
+
+// toggle dark/light mode
+let darkMode = true;
+
+const body = $("body");
+const p = $("p");
+const footer = $("footer");
+const topImage = $("#top");
+const header = $("header");
+const toggleLight = $("#toggle-light");
+const light = $(".light");
+const dark = $(".dark");
+light.hide();
+
+const toggle = () => {
+  if (darkMode) {
+    hideShow(dark, light);
+    body.css("background-color", "rgb(191,191,186)");
+    body.css("color", "rgb(38,38,38)");
+    p.css("color", "rgb(38,38,38)");
+    footer.css("color", "rgb(38,38,38)");
+  } else {
+    hideShow(light, dark);
+    body.css("background-color", "");
+    body.css("color", "rgb(191,191,186)");
+    p.css("color", "#f2f2eb");
+    footer.css("color", "#f2f2eb");
+  }
+  darkMode = !darkMode;
+};
+
+toggleLight.on("click", toggle);
+
+if (!isHome) {
+  const buttons = $("#project-menu").children();
+
+  const selectButton = (n) => {
+    return $(".project-button:nth-of-type("+n+")")
+  }
+
+  const selectProject = (n) => {
+    return $(".project:nth-of-type("+n+")");
+  };
+
+  // set default button and project
+  let activeButton = selectButton(1);
+  let activeProject = selectProject(1);
+
+  activeButton.css("background-color", hex.backgroundLight);
+  activeButton.css("border", `2px solid ${hex.backgroundDark}`);
+  activeButton.css("color", hex.backgroundDark);
+
+  const handleClick = (event) => {
+
+    const nextIndex = event.target.value;
+    const nextButton = selectButton(nextIndex);
+    const nextProject = selectProject(nextIndex);
+
+    activeButton.css("background-color", "");
+    activeButton.css("border", "");
+    activeButton.css("color", "");
+
+    nextButton.css("background-color", hex.backgroundLight);
+    nextButton.css("border", `2px solid ${hex.backgroundDark}`);
+    nextButton.css("color", hex.backgroundDark);
+
+    hideShow(activeProject, nextProject);
+
+    activeButton = nextButton;
+    activeProject = nextProject;
+  };
+
+  buttons.on("click", handleClick);
+
+}
